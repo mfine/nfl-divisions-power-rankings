4c831c55b68074655bc8319cffbd280855cdf7ce
Mark Fine <mark.fine@gmail.com>
2015-09-22 16:08:37 -0700

    Add std dev, headings.

--- Main.hs
+++ Main.hs
@@ -12,8 +12,14 @@ import System.Exit
 import qualified Data.Map.Strict as Map
 
 data Ranking = Ranking
-  { rank :: Int
-  , team :: String
+  { rRank :: Int
+  , rTeam :: String
+  } deriving (Eq, Show, Read)
+
+data Results = Results
+  { rDivision :: String
+  , rAverage  :: Double
+  , rStdDev   :: Double
   } deriving (Eq, Show, Read)
 
 afcEast, afcWest, afcSouth, afcNorth :: String
@@ -73,11 +79,11 @@ divisions = Map.fromList
 
 ranking :: Parser Ranking
 ranking = do
-  rank' <- many1 digit
+  rank <- many1 digit
   many space
-  team' <- many1 alphaNum
+  team <- many1 alphaNum
   newline
-  return $ Ranking (read rank' :: Int) team'
+  return $ Ranking (read rank :: Int) team
 
 parser :: FilePath -> IO [Ranking]
 parser file =
@@ -92,17 +98,23 @@ groupSort kvs = Map.toList (Map.fromListWith (++) [(k, [v]) | (k, v) <- kvs])
 average :: [Int] -> Double
 average xs = realToFrac (sum xs) / genericLength xs
 
-rankings :: FilePath -> IO [(String, Double)]
+stdDev :: [Int] -> Double
+stdDev xs = sqrt $ sum (map f xs) / genericLength xs where
+  f x = (realToFrac x - average xs) ** 2
+
+rankings :: FilePath -> IO [Results]
 rankings file = do
   rankings' <- parser file
-  return $ sortBy (comparing snd) $ map g $ groupSort $ map f rankings' where
+  return $ sortBy (comparing rAverage) $ map (uncurry g) $ groupSort $ map f rankings' where
     f (Ranking r t) = (fromJust $ Map.lookup t divisions, r)
-    g (d, r) = (d, average r)
+    g d r = Results d (average r) (stdDev r)
 
 main :: IO ()
 main = do
   args <- getArgs
   forM_ args $ \file -> do
     rankings' <- rankings file
+    printf "%-10s %10s %10s\n" ("Division" :: String) ("Mean" :: String) ("Std Dev" :: String)
+    printf "%-10s %10s %10s\n" ("--------" :: String) ("----" :: String) ("-------" :: String)
     forM_ rankings' $ \ranking' ->
-      printf "%-10s %5.2f\n" (fst ranking') (snd ranking')
+      printf "%-10s %10.2f %10.2f\n" (rDivision ranking') (rAverage ranking') (rStdDev ranking')
