2d5c4144e86a0ae5a0b0553425ad64ce21b428b1
Mark Fine <mark.fine@gmail.com>
2015-09-22 21:51:42 -0700

    Cleanup.

--- Main.hs
+++ Main.hs
@@ -84,14 +84,12 @@ ranking = do
   many space
   team <- many1 alphaNum
   newline
-  return $ Ranking (read rank :: Int) team
+  return $ Ranking (read rank) team
 
 parser :: FilePath -> IO [Ranking]
 parser file =
-  parseFromFile (many ranking) file >>= either report return where
-    report err = do
-      print err
-      exitFailure
+  parseFromFile (count (length $ Map.keys divisions) ranking) file >>= either report return where
+    report err = print err >> exitFailure
 
 groupSort :: Ord k => [(k, v)] -> [(k, [v])]
 groupSort kvs = Map.toList (Map.fromListWith (++) [(k, [v]) | (k, v) <- kvs])
@@ -103,10 +101,10 @@ stdDev :: [Int] -> Double
 stdDev xs = sqrt $ sum (map f xs) / genericLength xs where
   f x = (realToFrac x - average xs) ** 2
 
-rankings :: FilePath -> IO [Results]
-rankings file = do
-  rankings' <- parser file
-  return $ sortBy (comparing rAverage) $ map (uncurry g) $ groupSort $ map f rankings' where
+results :: FilePath -> IO [Results]
+results file = do
+  rankings <- parser file
+  return $ sortBy (comparing rAverage) $ map (uncurry g) $ groupSort $ map f rankings where
     f (Ranking r t) = (fromJust $ Map.lookup t divisions, r)
     g d r = Results d (average r) (stdDev r)
 
@@ -114,8 +112,8 @@ main :: IO ()
 main = do
   args <- getArgs
   forM_ args $ \file -> do
-    rankings' <- rankings file
     printf "%-10s %10s %10s\n" ("Division" :: String) ("Mean" :: String) ("Std Dev" :: String)
     printf "%-10s %10s %10s\n" ("--------" :: String) ("----" :: String) ("-------" :: String)
-    forM_ rankings' $ \ranking' ->
-      printf "%-10s %10.2f %10.2f\n" (rDivision ranking') (rAverage ranking') (rStdDev ranking')
+    rs <- results file
+    forM_ rs $ \r ->
+      printf "%-10s %10.2f %10.2f\n" (rDivision r) (rAverage r) (rStdDev r)
